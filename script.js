e9aa32266917597827759d8d70d1ee83d0fb5bff
lackerpack <thapaanil2007@gmail.com>
2022-07-20 17:15:48 -0500

    Fix qutoient function

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function product(...nums) {
 
 function quotient(...nums){
     let quotient = nums[0];
-    for (let i = 1; i < nums[0].length; i++){
-        quotient /= (nums[0])[i];
+    for (let i = 1; i < nums.length; i++){
+        quotient /= nums[0][i];
     }
     return quotient;
 }
