f7eed665ddc716f0fa45e912e7e97042c656491f
lackerpack <thapaanil2007@gmail.com>
2022-07-21 12:45:35 -0500

    Parse strings as floats

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sum(...nums) {
   console.log(nums);
   let sum = 0;
   for (let i = 0; i < nums.length; i++){
-    sum += nums[i];
+    sum += parseFloat(nums[i]);
   }
   return sum;
 };
@@ -11,10 +11,10 @@ function difference(...nums) {
 	let difference = 0;
   for (let num in nums){
     if (num == 0){
-      difference += nums[num];
+      difference += parseFloat(nums[num]);
     }
     else{
-      difference -= nums[num];
+      difference -= parseFloat(nums[num]);
     }
   }
   return difference;
@@ -23,7 +23,7 @@ function difference(...nums) {
 function product(...nums) {
     let product = 1;
     for (let i = 0; i < nums.length; i++){
-      product *= nums[i];
+      product *= parseFloat(nums[i]);
     }
     return product;
 };
@@ -31,7 +31,7 @@ function product(...nums) {
 function quotient(...nums){
     let quotient = nums[0];
     for (let i = 1; i < nums.length; i++){
-        quotient /= nums[i];
+        quotient /= parseFloat(nums[i]);
     }
     return quotient;
 }
